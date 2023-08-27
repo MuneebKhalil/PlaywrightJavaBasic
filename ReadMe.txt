fe170c166b85313c6766b477336242cc7cfc0720
muneeb <muneebkhalil96@gmail.com>
2023-08-27 12:38:49 +0500

    All files added

--- ReadMe.txt
+++ ReadMe.txt
@@ -1,1 +1,2 @@
 mvn exec:java -e -D exec.mainClass=com.microsoft.playwright.CLI -D exec.args="codegen demo.playwright.dev/todomvc"
+playwright.$("")  to create locators in codegen in inspect console
--- src/test/java/ReactJsSelector.java
+++ src/test/java/ReactJsSelector.java
@@ -2,6 +2,9 @@ import com.microsoft.playwright.*;
 
 public class ReactJsSelector {
     public static void main(String[] args) {
+        //Extension : React Developer tools --> in extension use components
+
+
         Playwright playwright = Playwright.create();
         BrowserType.LaunchOptions launchOptions = new BrowserType.LaunchOptions();
 
@@ -9,6 +12,8 @@ public class ReactJsSelector {
         Browser browser = playwright.chromium().launch(launchOptions);
         BrowserContext browserContext = browser.newContext();
         Page page = browserContext.newPage();
+        page.navigate("https://www.netflix.com/browse");
+        page.locator("_react=p[name='email'] >> input");
 
     }
 }
